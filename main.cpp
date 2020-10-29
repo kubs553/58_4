aa2843df46ec5936da6acf51b046226d7d53f4a2
kubs553 <33002823+kubs553@users.noreply.github.com>
2020-10-29 12:27:29 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <int> v;
+
+int bin(string x)
+{
+    int ans = 0;
+    int licz = 1;
+    for(int i = x.size()-1; i >= 0; i--)
+    {
+        if(x[i] == '-')
+            ans *= -1;
+        if(x[i] == '1')
+            ans += licz;
+        licz *= 2;
+    }
+    return ans;
+}
+
+int oblicz(int x, int y)
+{
+    int ans = (x - y) * (x - y);
+    return ans;
+}
+
+int main()
+{
+    ifstream file1("dane_systemy1.txt");
+    if (file1.good())
+    {
+        string x;
+        bool wczytaj = false;
+        while(file1 >> x)
+        {
+            if(wczytaj)
+                v.push_back(bin(x));
+            wczytaj = !wczytaj;
+        }
+
+        int maxx = INT_MIN;
+        for(int i = 0; i < v.size(); i++)
+        {
+            for(int j = 0; j < v.size(); j++)
+            {
+                int ans = oblicz(v[i], v[j]);
+                maxx = max(maxx, ans);
+            }
+        }
+        cout << "Najwiekszy skok temperatury: " << maxx;
+    }
+
+    return 0;
+}
